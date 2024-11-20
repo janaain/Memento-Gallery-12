d91e5b77cac2844f8b0e8c11cdcb6411a6862390
MrSeguro <152495876+MrSeguro@users.noreply.github.com>
2024-11-20 19:46:18 +0000

    asdfg

--- javascript/viewphoto.js
+++ javascript/viewphoto.js
@@ -12,6 +12,27 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+/* Tags */
+
+document.addEventListener("DOMContentLoaded", function () {
+    const addTagOption = document.getElementById("addTagOption"); // Opção na sidebar
+    const tagContainer = document.getElementById("tagContainer"); // Container das tags
+
+    addTagOption.addEventListener("click", function (event) {
+        event.preventDefault(); // Previne o comportamento padrão do link
+
+        // Cria um novo input para a tag
+        const newTagInput = document.createElement("input");
+        newTagInput.type = "text";
+        newTagInput.className = "action-input";
+        newTagInput.placeholder = `Tag ${tagContainer.childElementCount + 1}`;
+
+        // Adiciona o novo input ao container
+        tagContainer.appendChild(newTagInput);
+    });
+});
+
+
 
 
 
